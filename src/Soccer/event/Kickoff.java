c9217f7570e73c0c59c42846373a3ac13dabde93
Antony Fayez <161525125+AntonyFayez@users.noreply.github.com>
2025-08-23 02:16:26 +0300

    Kickoff.java

--- src/Soccer/event/Kickoff.java
+++ src/Soccer/event/Kickoff.java
@@ -8,18 +8,7 @@ public class Kickoff extends GameEvent {
     }
 
     @Override
-    public String toString() {      //This means when toString() is called on the object, it returns that string at that moment.
-                                    //toString is a method that returns a String, it does NOT store the string anywhere in the object.
-                                   //What if I override toString multiple times in subclasses?
-        //Every subclass can override toString().
-
-        //The last overriding method (closest to the object's actual class) is the one invoked.
-
-        //If a class does not override toString(), the method from its superclass is used instead.
-
-        //The initial toString() method from Object class outputs something like ClassName@65ab7765—a string based on memory address, which is typically not human-readable.
-
-        //Your overrides replace that default behavior with meaningful output.
+    public String toString() {      
         return "Kickoff by " + theTeam + " at " + theTime + " minutes";
     }
 }
